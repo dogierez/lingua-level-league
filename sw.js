eb42bff4b5129759037ca8f4cdc03472e33dc186
dogierez <78384991+dogierez@users.noreply.github.com>
2026-06-02 23:36:17 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'grammar-league-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './logo.png'
+];
+
+// Install the service worker and cache the assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Intercept network requests and serve from cache if available
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return the cached response if found, otherwise fetch from the network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Update the service worker and clear old caches if the cache name changes
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
